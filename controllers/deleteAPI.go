abc139a205b2beaea690ba17fad2b22a03d5d9da
sakda <sakda.pr@inet.co.th>
2020-08-11 15:45:41 +0700

    [Edited]: api delete

--- controllers/deleteAPI.go
+++ controllers/deleteAPI.go
@@ -1,13 +1,30 @@
 package controllers
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"restfull/configs"
+	"restfull/models"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // DeleteQuery func NOTE delete query func
 func DeleteQuery(c *gin.Context) {
-	username := c.Query("username")
-	c.JSON(http.StatusOK, gin.H{"message": username})
+	// connect to db
+	configs.ConnectionMongoDB()
+
+	body := new(models.User)
+	c.BindJSON(&body)
+	collection := configs.ClientMongoDB.Database("demoGolang").Collection("user")
+	_, err := collection.DeleteOne(context.TODO(), bson.M{"_id": body.ID})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Disconnect db
+	defer configs.DisconnectMongoDB()
+	c.JSON(http.StatusOK, gin.H{"message": "deleted"})
 }
